b13cbc02bffbf310f861f6c2523e10948af31f47
raihan <raihanulmcse@mail.com>
2022-03-01 20:52:10 +0800

    Additional part added

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const mobileFind = () => {
 };
 
 const phoneList = (phones) => {
-	for (const phone of phones) {
+	const first20Data = phones.slice(0, 20);
+	for (const phone of first20Data) {
 		const main = document.getElementById("phones-container");
+
 		const div = document.createElement("div");
+		div.className = "col-lg-4 mb-3";
 		div.innerHTML = `  <div class="card border">
         <div class="phone-details">
             <img  class=" mt-3 w-25" src="${phone.image}" alt="">
@@ -38,7 +41,8 @@ const phoneValueLoaded = (info) => {
     <h5><span class="text-primary"> ChipSet:</span> ${info.mainFeatures.chipSet}</h5>
     <h5><span class="text-primary">Memory:</span> ${info.mainFeatures.memory}</h5>
     <h5><span class="text-primary"> Display-Size:</span> ${info.mainFeatures.displaySize}</h5>
-    <h5><span class="text-primary"> Other:</span> ${info.others.WALN}</h5>
+    <h5><span class="text-primary"> Others:</span> ${info.others.Bluetooth}</h5>
+    <h5><span class="text-primary"> Sensor:</span> ${info.mainFeatures.sensors}</h5>
     
     </div>
     `;

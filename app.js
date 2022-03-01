e9ddf369b1c6d85f0fbc1a2518a9aab4e603b993
raihan <raihanulmcse@mail.com>
2022-03-01 18:54:17 +0800

    phone information been added

--- app.js
+++ app.js
@@ -13,16 +13,17 @@ const phoneList = (phones) => {
 		const div = document.createElement("div");
 		div.innerHTML = `  <div class="card border">
         <div class="phone-details">
-            <img  class=" w-25" src="${phone.image}" alt="">
-            <h5>Brand:${phone.brand}</h5>
-            <h5>Phone Name: ${phone.phone_name}</h5>
-            <button class="btn btn-success mb-4">Details</button>
+            <img  class=" mt-3 w-25" src="${phone.image}" alt="">
+            <h5 class=" mt-3">Brand: ${phone.brand}</h5>
+            <h5 class=" mt-3">Phone: ${phone.phone_name}</h5>
+            <button class="btn btn-success mb-4" onclick="phoneInfo('${phone.slug}')">Details</button>
 
         </div>
     </div>`;
 		main.appendChild(div);
-		console.log(phone);
 	}
+};
 
-	// console.log(phones);
+const phoneInfo = (info) => {
+	console.log(info);
 };

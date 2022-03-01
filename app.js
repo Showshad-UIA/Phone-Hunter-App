fc16b052469e145e27b4fbb1acc5474c10c23752
raihan <raihanulmcse@mail.com>
2022-03-01 19:37:50 +0800

    singel phone data displayed

--- app.js
+++ app.js
@@ -17,13 +17,30 @@ const phoneList = (phones) => {
             <h5 class=" mt-3">Brand: ${phone.brand}</h5>
             <h5 class=" mt-3">Phone: ${phone.phone_name}</h5>
             <button class="btn btn-success mb-4" onclick="phoneInfo('${phone.slug}')">Details</button>
-
         </div>
     </div>`;
 		main.appendChild(div);
 	}
 };
+const phoneInfo = (phoneId) => {
+	const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => phoneValueLoaded(data.data));
+};
 
-const phoneInfo = (info) => {
+const phoneValueLoaded = (info) => {
+	document.getElementById("phone-Container").innerHTML = `
+    <div>
+    <img class="w-25" src="${info.image}" alt="">
+    <h3><span class="text-primary"> Name:</span> ${info.name}</h3>
+    <h5><span class="text-primary"> Release-Date:</span> ${info.releaseDate}</h5>
+    <h5><span class="text-primary"> ChipSet:</span> ${info.mainFeatures.chipSet}</h5>
+    <h5><span class="text-primary">Memory:</span> ${info.mainFeatures.memory}</h5>
+    <h5><span class="text-primary"> Display-Size:</span> ${info.mainFeatures.displaySize}</h5>
+    <h5><span class="text-primary"> Other:</span> ${info.others.WALN}</h5>
+    
+    </div>
+    `;
 	console.log(info);
 };

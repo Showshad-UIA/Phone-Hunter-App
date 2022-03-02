d7eef80b3059a9447a8848429eb9a28f33c68745
raihan <raihanulmcse@mail.com>
2022-03-02 20:45:48 +0800

    latest updated

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const mobileFind = () => {
 	const input = document.getElementById("search-box");
 	const searchValue = input.value;
 	input.value = "";
+
 	// error handling
+
 	if (searchValue == "") {
 		error.innerText = "please input a brand name!!";
 		input.value = "";
@@ -29,12 +31,9 @@ const mobileFind = () => {
 const phoneList = (phones) => {
 	const first20Data = phones.slice(0, 20);
 	for (const phone of first20Data) {
-		if (phone !== first20Data) {
-			error.innerText = "Item not found";
-		} else {
-			const div = document.createElement("div");
-			div.className = "col-lg-4 mb-3";
-			div.innerHTML = `  <div class="card border">
+		const div = document.createElement("div");
+		div.className = "col-lg-4 mb-3";
+		div.innerHTML = `  <div class="card border">
         <div class="phone-details">
             <img  class=" mt-3 w-50 mb-2" src="${phone.image}" alt="">
             <h5 class=" mt-3">Brand: ${phone.brand}</h5>
@@ -42,8 +41,7 @@ const phoneList = (phones) => {
             <button class="btn btn-success mb-4" onclick="phoneInfo('${phone.slug}')">See Details</button>
         </div>
     </div>`;
-			main.appendChild(div);
-		}
+		main.appendChild(div);
 	}
 };
 
@@ -59,16 +57,16 @@ const phoneInfo = (phoneId) => {
 // display the phone details
 const phoneValueLoaded = (info) => {
 	document.getElementById("phone-Container").innerHTML = `
-    <div >
-    <img class="w-50" src="${info.image}" alt="">
-    <p class="mt-5"><span class="text-primary"> Name:</span> ${info.name}</p>
-    <p><span class="text-primary"> Release:</span> ${info.releaseDate}</p>
-    <p><span class="text-primary"> ChipSet:</span> ${info.mainFeatures.chipSet}</p>
-    <p><span class="text-primary">Memory:</span> ${info.mainFeatures.memory}</p>
-    <p><span class="text-primary"> Display-Size:</span> ${info.mainFeatures.displaySize}</p>
-    <p><span class="text-primary"> Others:</span> ${info.others.WLAN}</p>
-    <p><span class="text-primary"> Sensor:</span> ${info.mainFeatures.sensors}</p>
-    
-    </div>
+			<div >
+			<img class="w-50" src="${info.image}" alt="">
+			<p class="mt-5"><span class="text-primary"> Name:</span> ${info.name}</p>
+			<p><span class="text-primary"> Release:</span> ${info.releaseDate}</p>
+			<p><span class="text-primary"> ChipSet:</span> ${info.mainFeatures.chipSet}</p>
+			<p><span class="text-primary">Memory:</span> ${info.mainFeatures.memory}</p>
+			<p><span class="text-primary"> Display-Size:</span> ${info.mainFeatures.displaySize}</p>
+			<p><span class="text-primary"> Others:</span> ${info.others.WLAN}</p>
+			<p><span class="text-primary"> Sensor:</span> ${info.mainFeatures.sensors}</p>
+			
+			</div>
     `;
 };

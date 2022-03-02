6e225cb7e92396d67fc6d97184ee66fb1505df87
raihan <raihanulmcse@mail.com>
2022-03-02 18:49:05 +0800

    code modified

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const main = document.getElementById("phones-container");
+const error = document.getElementById("error");
 const mobileFind = () => {
 	document.getElementById("phones-container").innerHTML = "";
 	document.getElementById("spinner").style.display = "block";
@@ -6,8 +7,6 @@ const mobileFind = () => {
 	const searchValue = input.value;
 	input.value = "";
 	// error handling
-
-	const error = document.getElementById("error");
 	if (searchValue == "") {
 		error.innerText = "please input a brand name!!";
 		input.value = "";
@@ -26,13 +25,16 @@ const mobileFind = () => {
 		error.innerHTML = "";
 	}
 };
-
+// sort data from the list
 const phoneList = (phones) => {
 	const first20Data = phones.slice(0, 20);
 	for (const phone of first20Data) {
-		const div = document.createElement("div");
-		div.className = "col-lg-4 mb-3";
-		div.innerHTML = `  <div class="card border">
+		if (phone !== first20Data) {
+			error.innerText = "Item not found";
+		} else {
+			const div = document.createElement("div");
+			div.className = "col-lg-4 mb-3";
+			div.innerHTML = `  <div class="card border">
         <div class="phone-details">
             <img  class=" mt-3 w-50 mb-2" src="${phone.image}" alt="">
             <h5 class=" mt-3">Brand: ${phone.brand}</h5>
@@ -40,13 +42,15 @@ const phoneList = (phones) => {
             <button class="btn btn-success mb-4" onclick="phoneInfo('${phone.slug}')">See Details</button>
         </div>
     </div>`;
-		main.appendChild(div);
+			main.appendChild(div);
+		}
 	}
 };
 
 // see details by using phone id
 const phoneInfo = (phoneId) => {
 	const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+
 	fetch(url)
 		.then((res) => res.json())
 		.then((data) => phoneValueLoaded(data.data));
@@ -57,13 +61,13 @@ const phoneValueLoaded = (info) => {
 	document.getElementById("phone-Container").innerHTML = `
     <div >
     <img class="w-50" src="${info.image}" alt="">
-    <h3 class="mt-5"><span class="text-primary"> Name:</span> ${info.name}</h3>
-    <h5><span class="text-primary"> Release-Date:</span> ${info.releaseDate}</h5>
-    <h5><span class="text-primary"> ChipSet:</span> ${info.mainFeatures.chipSet}</h5>
-    <h5><span class="text-primary">Memory:</span> ${info.mainFeatures.memory}</h5>
-    <h5><span class="text-primary"> Display-Size:</span> ${info.mainFeatures.displaySize}</h5>
-    <h5><span class="text-primary"> Others:</span> ${info.others.WLAN}</h5>
-    <h5><span class="text-primary"> Sensor:</span> ${info.mainFeatures.sensors}</h5>
+    <p class="mt-5"><span class="text-primary"> Name:</span> ${info.name}</p>
+    <p><span class="text-primary"> Release:</span> ${info.releaseDate}</p>
+    <p><span class="text-primary"> ChipSet:</span> ${info.mainFeatures.chipSet}</p>
+    <p><span class="text-primary">Memory:</span> ${info.mainFeatures.memory}</p>
+    <p><span class="text-primary"> Display-Size:</span> ${info.mainFeatures.displaySize}</p>
+    <p><span class="text-primary"> Others:</span> ${info.others.WLAN}</p>
+    <p><span class="text-primary"> Sensor:</span> ${info.mainFeatures.sensors}</p>
     
     </div>
     `;
